e3acafae1fd6c129dac8c4a8bdc6a67eb7b91264
ishikakaur64 <gagandeepkaur781@gmail.com>
2025-06-21 14:43:55 +0530

    JS codingal

---
+++ JS codingal homework/object data structure and array.js
@@ -0,0 +1,7 @@
+var Codingal = ["green", "red", "pink"];
+console.log(Codingal[1]);
+
+var fruits = ["Orange", "Apple", "Mango"];
+console.log(fruits.join("*"));
+fruits.pop();
+console.log(fruits);
