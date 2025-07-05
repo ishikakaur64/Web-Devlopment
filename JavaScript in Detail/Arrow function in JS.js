22f0febecd871252b11a83fe4e0095db6897d693
ishikakaur64 <gagandeepkaur781@gmail.com>
2025-07-05 16:54:43 +0530

    Java script in Detail

---
+++ JavaScript in Detail/Arrow function in JS.js
@@ -0,0 +1,2 @@
+add = (a,b)=> a+b;
+console.log(add(5,8));
---
+++ JavaScript in Detail/Convert, Search and Replace string.js
@@ -0,0 +1,11 @@
+var str = "hello how are you doing for the day";
+var search_position = str.search("doing");
+
+console.log(search_position);
+
+var txt = str.replace("how are","how are you");
+
+console.log(txt);
+
+var x = 70;
+console.log(typeof (x))
